daaeb45af4db62d57c3ad288728adaccc46916f3
梁宵 <liangxiao@sunlands.com>
2018-12-29 11:50:44 +0800

    1.增加deep and cross 类 2.增加deep and cross network结构图

---
+++ deep_and_cross.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/29 11:37
+# @Author  : liangxiao
+# @Site    : 
+# @File    : deep_and_cross.py
+# @Software: PyCharm
+
+
+class deep_and_cross(object):
+    def __init__(self):
+        pass
+
+    def printshit(self):
+        print "shit"
+
+
+if __name__ == '__main__':
+    a = deep_and_cross()
+    a.printshit()
